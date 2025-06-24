cf827cb6f598e0556b605901ce9cdfd525b67823
Jerry Mindek <jerry.mindek@gmail.com>
2025-06-23 21:36:08 -0400

    Add tests for game history store.

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(customJestConfig);
---
+++ jest.unit.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
